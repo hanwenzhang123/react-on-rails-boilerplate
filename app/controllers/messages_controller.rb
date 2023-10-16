4719e5abc648bfb3b042466a9b4e2bb4f70711f9
JuanDBta <simplebetsas@gmail.com>
2023-10-16 16:44:09 -0500

    Created greetings messages

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,20 @@
+class MessagesController < ApplicationController
+  def new
+    @message = Message.new
+  end    
+
+  def create
+    @message = Message.new(message_params)
+    if @message.save
+      redirect_to messages_path
+    else
+      render 'new'
+    end
+  end
+    
+  private
+    
+  def message_params
+    params.require(:message).permit(:text)
+  end
+end
